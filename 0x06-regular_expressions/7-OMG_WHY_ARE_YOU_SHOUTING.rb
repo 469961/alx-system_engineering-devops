0c38dea961d423a4b40cedf823b8b77484902ea2
“469961” <mphomeshackslk@gmail.com>
2023-05-15 18:56:18 -0700

    0x05-processes_and_signals

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# This script should output: [SENDER],[RECEIVER],[FLAGS]
-
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+puts ARGV[0].scan(/[A-Z]*/).join
