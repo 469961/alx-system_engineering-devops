d2f1dd65e6e327a4bc2d65f115a5387982d7793d
“469961” <mphomeshackslk@gmail.com>
2023-07-21 17:10:33 -0700

    0x12-web_stack_debugging_2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Installs flask from pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
