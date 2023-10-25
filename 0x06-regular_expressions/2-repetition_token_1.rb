f122f730b9200f7a2a8e326e8e4d78cc440e2cfc
mpho seleke <117736807+469961@users.noreply.github.com>
2023-10-25 22:34:40 +0200

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match the given cases
-
-puts ARGV[0].scan(/hb?tn/).join
